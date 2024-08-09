5db81ff77d9585bf322ff5562cb053c6dd29dd32
Aakash Rajput <105713416+arajput108@users.noreply.github.com>
2024-08-09 09:11:52 +0530

    Commit- D16

--- 16.) Maximize Array Value After Rearrangement.js
+++ 16.) Maximize Array Value After Rearrangement.js
@@ -3,7 +3,6 @@
 // Note: Since the output could be large, print the answer modulo 109+7.
 
 // Examples :
-
 // Input : arr[] = [5, 3, 2, 4, 1]
 // Output : 40
 // Explanation: If we arrange the array as 1 2 3 4 5 then we can see that the minimum index will multiply with minimum number and maximum index will multiply with maximum number. So, 1*0 + 2*1 + 3*2 + 4*3 + 5*4 = 0+2+6+12+20 = 40 mod(109+7) = 40
