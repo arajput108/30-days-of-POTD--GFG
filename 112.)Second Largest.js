2a3dc6b744dbc62f8f5bea6e4bba8147aed24f21
Aakash Rajput <105713416+arajput108@users.noreply.github.com>
2024-11-15 04:55:09 +0530

    Commit- D112

---
+++ 112.)Second Largest.js
@@ -0,0 +1,82 @@
+// 112.) Given an array of positive integers arr[], return the second largest element from the array. 
+//       If the second largest element doesn't exist then return -1.
+
+// Note: The second largest element should not be equal to the largest element.
+
+// Examples:
+
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
+// Input: arr[] = [10, 10, 10]
+// Output: -1
+// Explanation: The largest element of the array is 10 and the second largest element does not exist.
+// Constraints:
+// 2 ≤ arr.size() ≤ 105
+// 1 ≤ arr[i] ≤ 105
+// <______________________________________________SOLUTION___________________________________________________>
+//{ Driver Code Starts
+// Initial Template for javascript
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split("\n").map(string => string.trim());
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    for (let i = 0; i < t; i++) {
+        const arr = readLine().split(' ').map(x => parseInt(x));
+        let obj = new Solution();
+        let ans = obj.getSecondLargest(arr);
+        console.log(ans);
+        console.log("~");
+    }
+}
+
+// } Driver Code Ends
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+
+
+// <___________________________________________________MAIN-SOLUTION_____________________________________________________>
+class Solution {
+    // Function returns the second largest element
+    getSecondLargest(arr) {
+        // Code Here
+        let largest = -1;
+        let secondLargest = -1;
+        
+        for (let num of arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num < largest) {
+                secondLargest = num;
+            }
+        }
+        
+        return secondLargest;
+    }
+}
+
+
+
+// Compilation Completed
+For Input:  12 35 1 10 34 1
+Your Output:  34
+Expected Output:  34
