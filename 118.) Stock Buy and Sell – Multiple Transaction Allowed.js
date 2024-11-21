b161dde4c5abaa12464b60cd7f5633d88bede618
Aakash Rajput <105713416+arajput108@users.noreply.github.com>
2024-11-21 06:24:09 +0530

    Commit- D118

---
+++ 118.) Stock Buy and Sell – Multiple Transaction Allowed.js
@@ -0,0 +1,85 @@
+The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+
+Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
+
+Examples:
+
+Input: prices[] = [100, 180, 260, 310, 40, 535, 695]
+Output: 865
+Explanation: Buy the stock on day 0 and sell it on day 3 => 310 – 100 = 210. Buy the stock on day 4 and sell it on day 6 => 695 – 40 = 655. Maximum Profit = 210 + 655 = 865.
+
+
+Input: prices[] = [4, 2, 2, 2, 4]
+Output: 2
+Explanation: Buy the stock on day 3 and sell it on day 4 => 4 – 2 = 2. Maximum Profit = 2.
+Constraints:
+1 <= prices.size() <= 105
+0 <= prices[i] <= 104
+
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        // let n = parseInt(readLine());
+        let arr = readLine().trim().split(" ").map((x) => parseInt(x));
+        let n = arr.length;
+        let obj = new Solution();
+        let res = obj.maximumProfit(arr);
+        console.log(res);
+        // console.log("~");
+    }
+}
+// } Driver Code Ends
+// <______________________________________________SOLUTION___________________________________________________>
+
+class Solution {
+    /**
+    * @param number[] prices
+
+    * @returns number
+    */
+    maximumProfit(prices) {
+        // code here
+         let maxProfit = 0;
+        for (let i = 1; i < prices.length; i++) {
+            // If the price increases compared to the previous day, add the profit
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+}
+
+
+
+
+// Compilation Completed
+For Input: 
+100 180 260 310 40 535 695
+Your Output: 
+865
+Expected Output: 
+865
