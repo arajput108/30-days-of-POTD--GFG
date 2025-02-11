6d5d223c871aabc46112b1e9487c3f02d4be3a1f
Aakash Rajput <105713416+arajput108@users.noreply.github.com>
2025-02-11 14:36:45 +0530

    Commit- D202

---
+++ 202.) Check for BST.java
@@ -0,0 +1,168 @@
+// 202.) Given the root of a binary tree. Check whether it is a BST or not.
+// Note: We are considering that BSTs can not contain duplicate Nodes.
+// A BST is defined as follows:
+
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+// Examples:
+
+// Input: root = [2, 1, 3, N, N, N, 5]
+
+
+// Output: true 
+// Explanation: The left subtree of every node contains smaller keys and right subtree of every node contains 
+// greater keys. Hence, the tree is a BST.
+// Input: root = [2, N, 7, N, 6, N, 9] 
+
+
+
+// Output: false 
+// Explanation: Since the node to the right of node with key 7 has lesser key value, hence it is not a valid BST.
+// Input: root = [10, 5, 20, N, N, 9, 25]
+
+
+// Output: false
+// Explanation: The node with key 9 present in the right subtree has lesser key value than root node.
+// Constraints:
+// 1 ≤ number of nodes ≤ 105
+// 1 ≤ node->data ≤ 109
+// <______________________________________________SOLUTION___________________________________________________>
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            if (g.isBST(root))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            t--;
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+
+// <___________________________________________________MAIN-SOLUTION_____________________________________________________>
+
+class Solution {
+    // Function to check whether a Binary Tree is BST or not.
+    boolean isBST(Node root) {
+          return isvalidBst(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+        // code here.
+    }
+        public  boolean isvalidBst(Node root,int min,int max){
+        if(root==null)return true;
+        if(root.data<=min||root.data>=max)return false;
+        
+        return isvalidBst(root.left,min,root.data)&&
+        isvalidBst(root.right,root.data,max);
+    }
+}
+
+
+
+// Compilation Completed
+For Input: 
+2 1 3 N N N 5
+Your Output: 
+true
+Expected Output: 
+true
