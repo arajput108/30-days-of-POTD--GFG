ca93678fe619a08a9f92b5999c111819fe53449c
Aakash Rajput <105713416+arajput108@users.noreply.github.com>
2025-05-21 23:50:48 +0530

    Commit- D296

---
+++ 296.) Kth Smallest Number in Multiplication Table.java
@@ -0,0 +1,59 @@
+// 296.) Given three integers m, n and k. Consider a grid of m * n, where mat[i][j] = i * j (1 based index). 
+//       The task is to return the kth smallest element in the m * n multiplication table.
+
+// Examples :
+
+// Input: m = 3, n = 3, k = 5
+// Output: 3
+// Explanation: 
+
+// The 5th smallest element is 3. 
+// Input: m = 2, n = 3, k = 6
+// Output: 6 
+// Explanation: [1, 2, 3][2, 4, 6]. The 6th smallest element is 6.
+// Constraints:
+// 1 <= m, n <= 3 * 104
+// 1 <= k <= m * n
+// <-------------------------------------------------SOLUTION-------------------------------------------------->
+class Solution {
+    public int kthSmallest(int m, int n, int k) {
+        // code here
+         int low = 1;
+        int high = m*n;
+        int ans = -1;
+        
+        while (low<=high) {
+            int mid = (low+high)/2;
+            int count = smallerThan(m, n, mid);
+            if (count < k) {
+                low = mid+1;
+            } else if (count >= k) {
+                high = mid-1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+    
+    public int smallerThan(int m, int n, int mid) {
+        int count = 0;
+        for (int i=1; i<=m; i++) {
+            int j = Math.min(mid/i, n);
+            count += j;
+        }
+        return count;
+    }
+}
+
+// Compilation Completed
+Input: 
+m
+3
+n
+3
+k
+5
+Your Output:
+3
+Expected Output:
+3
